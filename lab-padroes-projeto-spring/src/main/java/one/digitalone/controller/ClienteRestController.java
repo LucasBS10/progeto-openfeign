97c83c7cb8a3af5bbecda1795f6ba7053d22f608
LucasBS10 <lucasbernardo2604@gmail.com>
2023-04-15 20:04:45 -0300

    Initial commit

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalone/controller/ClienteRestController.java
@@ -0,0 +1,60 @@
+package one.digitalone.controller;
+
+/*import one.digitalone.model.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import one.digitalone.service.ClienteService;*/
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import one.digitalone.model.Cliente;
+import one.digitalone.service.ClienteService;
+
+
+@RestController
+@RequestMapping("clientes")
+public class ClienteRestController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping()
+    public ResponseEntity<Iterable<Cliente>> buscarTodos(){
+
+        return ResponseEntity.ok(clienteService.buscarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id){
+        return ResponseEntity.ok(clienteService.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> inserir(@RequestBody Cliente cliente) {
+        clienteService.inserir(cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+        clienteService.atualizar(id, cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        clienteService.deletar(id);
+        return ResponseEntity.ok().build();
+    }
+
+
+}
---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalone/model/Cliente.java
@@ -0,0 +1,50 @@
+package one.digitalone.model;
+
+//import javax.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    @ManyToOne
+    private Endereco endereco;
+
+    public Long getId() {
+
+        return this.id;
+    }
+
+    public void setId(Long id) {
+
+        this.id = id;
+    }
+
+    public String getNome() {
+
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+
+        return this.endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+
+        this.endereco = endereco;
+    }
+}
